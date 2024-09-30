c5e429071a44c1b161659caddc3993760cfa63e7
DavothSlayer <96688499+DavothSlayer@users.noreply.github.com>
2024-09-30 15:44:36 +0300

    Lean Tween added

---
+++ Assets/Scripts/UI/Tweener.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    public void ButtonPressBounce(GameObject gameObject)
+    {
+        LeanTween.scale(gameObject, new(1.15f, 1.15f, 1f), .1f).setEaseInBounce();
+        LeanTween.scale(gameObject, Vector3.one, .1f).setDelay(.1f);
+    }
+}
